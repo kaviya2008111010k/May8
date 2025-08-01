d21491ee324117007e2b4739f720beeabf37226c
kaviya2008111010k <Kavishkamadushan9596@gmail.com>
2025-08-01 15:46:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
+SESSION_ID: process.env.SESSION_ID || "NcBQ3ZpI#bkU6fp8IdFtx1-L2bxYqkkKh-rio3TV3wwTqV1O-8cI", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
-MODE: process.env.MODE || "public", // public,private,groups,inbox
+MODE: process.env.MODE || "private", // public,private,groups,inbox
 ALLWAYS_OFFLINE: process.env.ALLWAYS_OFFLINE || "false", // true,false
 LANG: process.env.LANG || "EN",
 
